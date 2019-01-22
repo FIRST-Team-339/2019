913d3b6f80ae7b269b86cc7043c1f0e488442a48
Robert Brown <rjbrown124@comcast.net>
2019-01-22 18:08:13 -0500

    about to pull

--- src/main/java/frc/Utils/AlignToDeposit.java
+++ src/main/java/frc/Utils/AlignToDeposit.java
@@ -6,58 +6,73 @@ import frc.HardwareInterfaces.KilroyEncoder;
 import frc.HardwareInterfaces.UltraSonic;
 import frc.Utils.drive.Drive;
 
-public class AlignToDeposit {
+public class AlignToDeposit
+{
 
-    public DriveWithCamera driveWithCamera = null;
+public DriveWithCamera driveWithCamera = null;
 
-    public Drive drive = null;
+public Drive drive = null;
 
-    public UltraSonic frontUltraSonic = null;
+public UltraSonic frontUltraSonic = null;
 
-    public KilroyEncoder leftFrontDriveEncoder = null;
+public KilroyEncoder leftFrontDriveEncoder = null;
 
-    public KilroyEncoder rightFrontDriveEncoder = null;
+public KilroyEncoder rightFrontDriveEncoder = null;
 
-    public AlignToDeposit() {
-        this.driveWithCamera = null;
-        this.drive = null;
-        this.frontUltraSonic = null;
-        this.frontUltraSonic = null;
-        this.leftFrontDriveEncoder = null;
-        this.rightFrontDriveEncoder = null;
-    }
+public AlignToDeposit ()
+{
+    this.driveWithCamera = null;
+    this.drive = null;
+    this.frontUltraSonic = null;
+    this.frontUltraSonic = null;
+    this.leftFrontDriveEncoder = null;
+    this.rightFrontDriveEncoder = null;
+}
 
-    public AlignToDeposit(DriveWithCamera driveWithCamera, Drive drive, UltraSonic frontUltraSonic) {
-        this.driveWithCamera = driveWithCamera;
-        this.drive = drive;
-        this.frontUltraSonic = frontUltraSonic;
+public AlignToDeposit (DriveWithCamera driveWithCamera, Drive drive,
+        UltraSonic frontUltraSonic)
+{
+    this.driveWithCamera = driveWithCamera;
+    this.drive = drive;
+    this.frontUltraSonic = frontUltraSonic;
 
-    }
+}
 
-    public AlignToDeposit(DriveWithCamera driveWithCamera, Drive drive, UltraSonic frontUltraSonic,
-            KilroyEncoder leftFrontDriveEncoder, KilroyEncoder rightFrontDriveEncoder) {
-        this.driveWithCamera = driveWithCamera;
-        this.drive = drive;
-        this.frontUltraSonic = frontUltraSonic;
-        this.leftFrontDriveEncoder = leftFrontDriveEncoder;
-        this.rightFrontDriveEncoder = rightFrontDriveEncoder;
+public AlignToDeposit (DriveWithCamera driveWithCamera, Drive drive,
+        UltraSonic frontUltraSonic,
+        KilroyEncoder leftFrontDriveEncoder,
+        KilroyEncoder rightFrontDriveEncoder)
+{
+    this.driveWithCamera = driveWithCamera;
+    this.drive = drive;
+    this.frontUltraSonic = frontUltraSonic;
+    this.leftFrontDriveEncoder = leftFrontDriveEncoder;
+    this.rightFrontDriveEncoder = rightFrontDriveEncoder;
 
-    }
+}
 
-    public boolean alignToDeposit(int override) {
+public boolean alignToDeposit (int override)
+{
 
-        switch (state) {
+
+
+    switch (state)
+        {
         case INIT:
-            if (USING_CAMERA) {
+            if (USING_CAMERA)
+                {
                 state = AlignToDepositState.ALIGN_BY_CAMERA;
-            } else {
+                } else
+                {
                 state = AlignToDepositState.ALIGN_BY_TAPE;
-            }
+                }
             break;
         case ALIGN_BY_CAMERA:
-            if (this.driveWithCamera.driveToTarget(ALIGN_BY_CAMERA_SPEED)) {
+            if (this.driveWithCamera
+                    .driveToTarget(ALIGN_BY_CAMERA_SPEED))
+                {
                 state = AlignToDepositState.ALIGN_BY_TAPE;
-            }
+                }
             break;
         case ALIGN_BY_TAPE:
             // alignByTape();
@@ -80,19 +95,20 @@ public class AlignToDeposit {
             break;
         }
 
-        return false;
+    return false;
+}
+
+AlignToDepositState state = AlignToDepositState.INIT;
+
+public enum AlignToDepositState
+    {
+INIT, ALIGN_BY_CAMERA, ALIGN_BY_TAPE, LIFT_MANIPULATOR, DRIVE, SCORE, STOP
     }
 
-    AlignToDepositState state = AlignToDepositState.INIT;
+private final double ALIGN_BY_CAMERA_SPEED = .3;
 
-    public enum AlignToDepositState {
-        INIT, ALIGN_BY_CAMERA, ALIGN_BY_TAPE, LIFT_MANIPULATOR, DRIVE, SCORE, STOP
-    }
+private final boolean USING_CAMERA = true;
 
-    private final double ALIGN_BY_CAMERA_SPEED = .3;
-
-    private final boolean USING_CAMERA = true;
-
-    private final double DISTANCE_AWAY_FROM_DEPOSIT = 10; // TODO
+private final double DISTANCE_AWAY_FROM_DEPOSIT = 10; // TODO
 
 }
