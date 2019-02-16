1a169738ed32af931ede37bc29fac6d41beda108
Robert Brown <rjbrown124@comcast.net>
2019-02-16 11:44:29 -0500

    format error

--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -1028,7 +1028,6 @@ public static final double FORKLIFT_DIVISOR = 4;
 private static double DRIVE_SPEED = .4;
 
 private static double TURN_SPEED = .4;
-
 // ================================
 // Variables
 // ================================
