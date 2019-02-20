f8d5a1d5902e6e084852dd789fe4d904964381db
Robert Brown <rjbrown124@comcast.net>
2019-02-19 21:06:08 -0500

    got rid of omments

--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -275,8 +275,7 @@ public static void periodic ()
     Hardware.depositGamePiece.printDebugStatements();
 
     // vision=====================================
-    System.out.println("ultrasonic values: "
-            + Hardware.frontUltraSonic.getDistanceFromNearestBumper());
+
     if (Hardware.visionHeightUpButton.get() == true
             && visionHeight < 2 && Hardware.telopTimer.get() > .25)
         {
