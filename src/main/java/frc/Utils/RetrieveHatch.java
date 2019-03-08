000ef24e4a9008ba7499cb7bfad4d960cb4d2131
Robert Brown <rjbrown124@comcast.net>
2019-03-07 20:51:05 -0500

    made new class to retrieve hatch from player station, will be a golden retriever

---
+++ src/main/java/frc/Utils/RetrieveHatch.java
@@ -0,0 +1,17 @@
+package frc.Utils;
+
+
+public class RetrieveHatch
+{
+public static enum retrievalState
+    {
+    STANDBY, START_RETRIEVAL, DOUBLE_CHECK_FORKLIFT_TO_POSITION, DELAY_ONE, DOUBLE_CHECK_ARM, DELAY_TWO, DRIVE_FORWARD, DELAY_THREE, LIFT_FORKLIFT_TO_GET_HATCH, DELAY_FOUR, BACKUP_SLOWLY, STOP, FINISH
+    }
+
+public void retrievalUpdate ()
+{
+
+}
+
+
+}
