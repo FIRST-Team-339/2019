b32c5f6ada13cdbf7f2e918e4749ea322d7b33b8
Robert Brown <rjbrown124@comcast.net>
2019-03-07 20:44:39 -0500

    Added setLiftPositionPrecise to forklift
    Meant to more accurately move the lift to a desired height.
    Added, but not fully tested or commented

--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -513,9 +513,20 @@ private static void coleTest ()
     // Hardware.manipulator.moveArmToPosition(45);
 
 
+
+
+
+    // if (Hardware.nextHigherLiftHeightButton.getCurrentValue())
+    // Hardware.lift.setLiftPositionPrecise(1.0, 30.0);
+
+    // if (Hardware.nextLowerLiftHeightButton.getCurrentValue())
+    // Hardware.lift.setLiftPositionPrecise(1.0, 5.0);
+
+
+
+
+
     // Manipulator
-
-
 
     // SmartDashboard.putString("intakeTriggerLeft",
     // "" + Hardware.intakeTriggerLeft.get());
@@ -528,7 +539,6 @@ private static void coleTest ()
 
     // SmartDashboard.putString("outtakeButtonRight",
     // "" + Hardware.outtakeButtonRight.get());
-
 
     // SmartDashboard.putString("Make break switch",
     // "" + Hardware.armBallDetector.isOn());
